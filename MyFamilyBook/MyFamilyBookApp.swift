96ef1e014392801de76904c60b8dbd0025cd572f
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-17 18:20:57 +0900

    Initial Commit

---
+++ MyFamilyBook/MyFamilyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFamilyBookApp.swift
+//  MyFamilyBook
+//
+//  Created by emi oiso on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct MyFamilyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
