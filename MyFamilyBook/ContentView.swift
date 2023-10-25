8766c4844a1ef5957bd9c1f7e856b67f6359063d
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-25 19:20:25 +0700

    .gitignoreとぽんちゃんの情報を登録するフォームを@ObservedObjectを使って作ったよ。

--- MyFamilyBook/ContentView.swift
+++ MyFamilyBook/ContentView.swift
@@ -7,18 +7,77 @@
 
 import SwiftUI
 
+class Ponta: ObservableObject{
+    @Published var name = ""
+    @Published var age = ""
+    @Published var wight = ""
+}
+
 struct ContentView: View {
+    //キーボード等の外部入力から値の初期値
+    @ObservedObject var ponta = Ponta()
+    @State var isShowInfoView = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("ぽんちゃんの情報を入力してください。")
+                .font(.headline)
+            //キーボード等の外部入力から値を受け取り、TextFieldの枠内に表示させる。
+            TextField("名前",text:$ponta.name)
+                //キーボード等の外部入力から英字を入力する際、小文字で入力できるようにしている。
+                .autocapitalization(.none)
+                //TextFieldの枠のコーナーを丸くなるようにしている。
+                .textFieldStyle(.roundedBorder)
+            
+            //キーボード等の外部入力から値を受け取り、TextFieldの枠内に表示させる。
+            TextField("年齢",text:$ponta.age)
+                //TextFieldの枠のコーナーを丸くなるようにしている。
+                .textFieldStyle(.roundedBorder)
+                //数字のみが入力できるキーパッドを出力している
+                .keyboardType(.numberPad)
+            
+            //キーボード等の外部入力から値を受け取り、TextFieldの枠内に表示させる。
+            TextField("体重",text:$ponta.wight)
+                //キーボード等の外部入力から英字を入力する際、小文字で入力できるようにしている。
+                .autocapitalization(.none)
+                //TextFieldの枠のコーナーを丸くなるようにしている。
+                .textFieldStyle(.roundedBorder)
+                //数字のみが入力できるキーパッドを出力している
+                .keyboardType(.numberPad)
+            
+            Button("決定"){
+                isShowInfoView = true
+            }
+            .sheet(isPresented: $isShowInfoView){
+                InfoView(ponta: ponta)
+            }
         }
+        .textFieldStyle(.roundedBorder)
         .padding()
     }
 }
 
+
+struct InfoView: View {
+    @ObservedObject var ponta: Ponta
+//    @Binding var name :String
+//    @Binding var age  :String
+//    @Binding var wight :String
+    
+    var body: some View{
+        VStack{
+            Text("【 ぽんちゃん情報 】")
+                .padding()
+            Text("名前:\(ponta.name)")
+            Text("年齢:\(ponta.age)")
+            Text("体重:\(ponta.wight)")
+            Button("決定"){
+            }
+        }
+        .textFieldStyle(.roundedBorder)
+        .padding()
+    }
+}
 #Preview {
     ContentView()
 }
